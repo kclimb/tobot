ca94c9c95c2f4c96174839dc74cab478b06be701
kclimb <22298376+kclimb@users.noreply.github.com>
2019-04-25 18:01:06 -0500

    Docstrings for commands
    Yikes at my documentation lately

--- src/commands.py
+++ src/commands.py
@@ -8,15 +8,29 @@ import random
 #import requests
 
 def sayhi():
+	"""
+	:)
+	"""
 	return 'hi :)'
 
 def sayhello(name):
+	"""
+	Yes, this bot does have multiple greeting commands. Saying hi is important.
+	"""
 	return 'hello '+name+'!'
 
 def saythanks():
+	"""
+	This bot is polite, dammit.
+	"""
 	return 'thanks'
 
 def settitle(title, user_type):
+	"""
+	Allows mods and broadcasters to set the title of the stream game.
+	Note that the 'title' parameter is specified by the chat message, but
+	user_type is supplied by this bot from parsing the chat message's metadata.
+	"""
 	f = open('mydata.txt')
 	headers = {
 		'Client-ID': '***REAL CLIENT ID HERE',
@@ -31,18 +45,41 @@ def settitle(title, user_type):
 	return 'Title is set to ' + title
 
 def title():
+	"""
+	Returns the title of the current game. Needs Twitch API access.
+	"""
 	return "Not implemented yet :("
 
 def nop():
+	"""
+	It does nothing. Leftover from when I thought it was a good idea to force
+	the bot to have a designated response for every type of chat message.
+	"""
 	return None
 
 def dr():
+	"""
+	Returns the name of the video game franchise, Danganronpa. Kind of.
+	"""
 	return random.choice(DANGAN) + ' ' + random.choice(RONPA)
 
 def wr():
+	"""
+	Fetches the World Record for the main speedrunning category of the current game.
+
+	Yes, I am very aware it just returns a string.
+	"""
 	return "it doesn't matter :)"
 
 def list_commands():
+	"""
+	Returns a list of all* commands currently supported by this bot.
+
+	*Well some things are more fun kept a secret**
+
+	**Ok sure it's not a secret if you just look at the commands map. But where's
+	the fun in that? Killjoy.
+	"""
 	retstring = ''
 	for guy in DEFAULT_COMMANDS.keys():
 		if guy.startswith('!'):
@@ -54,7 +91,7 @@ def list_commands():
 # Note there may be more actual arguments that toburobo needs to supply (from message headers, credentials, etc)
 DEFAULT_COMMANDS = {'!commands':list_commands,'!dr':dr,'!hi':sayhi,'!hello':sayhello,'!title':title,'!wr':wr,'gl':saythanks}
 DEFAULT_ARGC = {'!commands':0,'!dr':0,'!hi': 0,'!hello':1,'settitle':1,'!title':0,'!wr':0,'gl':0}
-MIN_ARGC = {} #{'!title':0}
+MIN_ARGC = {} #{'!title':0} This was a terrible idea should probably delete this but you never know, huh
 
 # Other handy data things
 DANGAN = ['danger', 'doggone', 'draugr', 'dagnabbit', 'dungeon', 'drumbo', 'dagger', 'dansgame', 'daenerys', 'dungarees', 'dunsparce']
